74d7d485b10e4e3f7e39c09dd39a9167933759e2
Jairo Herrera <jairoehr@gmail.com>
2025-05-11 16:24:10 -0500

    feat: enhance ContractService to integrate Redis for caching and deletion of juridical persons

--- src/services/contract.service.ts
+++ src/services/contract.service.ts
@@ -1,4 +1,4 @@
-import { ConflictError, CustomError, InternalServerError } from '@utils/index';
+import { ConflictError, CustomError, InternalServerError, redisClient } from '@utils/index';
 import { IContractModel, IContractService, IJuridicalPerson } from 'types';
 
 export class ContractService implements IContractService {
@@ -14,6 +14,8 @@ export class ContractService implements IContractService {
       });
       if (existJuridicalPerson) throw new ConflictError('Juridical person already exists');
       await this.#contractModel.createJuridicalPerson({ data });
+      const redisKey = 'juridicalPersonArray';
+      await redisClient.del(redisKey);
       return;
     } catch (error) {
       if (error instanceof CustomError) throw error;
@@ -21,12 +23,19 @@ export class ContractService implements IContractService {
     }
   }
 
-  getAllJuridicalPerson(): Promise<any[]> {
+  async getAllJuridicalPerson(): Promise<any[]> {
     try {
-      return this.#contractModel.getAllJuridicalPerson();
+      const redisKey = 'juridicalPersonArray';
+      const redisData = await redisClient.get(redisKey);
+      if (redisData) return JSON.parse(redisData);
+      const juridicalPersonArray = await this.#contractModel.getAllJuridicalPerson();
+      await redisClient.set(redisKey, JSON.stringify(juridicalPersonArray), {
+        expiration: { type: 'EX', value: 60 * 60 * 24 },
+      });
+      return juridicalPersonArray;
     } catch (error) {
       if (error instanceof CustomError) throw error;
-      throw new InternalServerError('Error getting all juridical persons');
+      throw new InternalServerError('Error getting all juridical persons please try again');
     }
   }
 }
